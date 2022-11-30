f77f4bb938f7470b42168c2ccec4ec8532fcd21a
riptide-rv <rohitramdas2003@gmail.com>
2022-11-30 21:45:24 +0530

    ''

--- FlaskMarket/market.py
+++ FlaskMarket/market.py
@@ -1,7 +1,19 @@
 
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
 
+
+class Item(db.Model):
+  name = db.Column(db.String(length=30),nullable=False,unique=True)
+  price = db.Column(db.Integer(),nullable=False)
+  barcode = db.Column(db.String(length=12),nullable=False,unique=True)
+  description = db.Column(db.String(length=1024),nullable=False,unique=True)
+  id = db.Column(db.Integer(),primary_key=True)
+  def __repr__(self) -> str:
+    return f'Item{self.name}'
 @app.route("/")
 @app.route("/home")
 def home_page():
@@ -9,10 +21,6 @@ def home_page():
     
 
 @app.route('/market')
-def marker_page():
-  items = [
-    {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-    {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-    {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-  ]
+def market_page():
+  items = Item.query.all()
   return render_template('market.html', items=items)
